2dd056ddf81adabc44daecbc11d5eac5e9aec87c
Marko Wallin <marko.wallin@iki.fi>
2012-01-13 16:18:59 +0200

    fix e_sieve clean solution

--- python/euler-3_primefactor.py
+++ python/euler-3_primefactor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
-# What is the largest prime factor of the number 317584931803?
+# What is the largest prime factor of the number 600851475143?
 #
 # Can be found eg. by Trial Division: http://en.wikipedia.org/wiki/Trial_division
 #
@@ -17,7 +17,7 @@ def e_sieve ( max ):
 	base = 2
 	product = 2
 	result = [1]
-	candidates = xrange(2, max)
+	candidates = range(2, max)
 
 	while candidates:
 		while product <= max:
@@ -37,8 +37,8 @@ def e_sieve ( max ):
 #
 def isprime(n):
    for x in xrange(2, int(floor(n**0.5)) + 1):
-      if not n % x:
-         return False
+        if not n % x:
+                return False
    return True
 
 # Trial Division
@@ -47,28 +47,26 @@ def isprime(n):
 def trial_division( n ):
         print "n: " + str(n)
         if n == 1: return [1]
-        #primes = e_sieve(int(n**0.5) + 1)
+        #primes = e_sieve(int(floor(n**0.5)) + 1)
         #print "primes: " + str(primes)
         factors = []
 
         for s in range(2,int(floor(sqrt(n)))):
         #for s in primes:
                 if n % s == 0:
-                        factors.append(s)
+                        if isprime(s):
+                                factors.append(s)
 
         return factors
 
 # Main
 # 
-#n = 13195
+# n = 13195
 n = 600851475143
 
 primes = trial_division(n)
-for x in primes:
-        if isprime(x):
-                last = x
 
 print "Primes: " + str(primes)
-print "Largest prime factor for " + str(n) + ": " +str(last)
+print "Largest prime factor for " + str(n) + ": " +str(primes[-1])
 
 
